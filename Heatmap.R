30f96ae8e4a6347e7b2cd80e3b7ada91f01f5ee6
Peter Arnesen <peterarnesen@outlook.com>
2022-01-25 22:08:06 +0100

    Finished

--- Heatmap.R
+++ Heatmap.R
@@ -4,6 +4,17 @@ library(ggplot2)
 library(reshape)
 
 DfForHM <- subset(DfForHM, select = -X )
-pdf("testsfgege.pdf", height=10, width=15)
+
+DfMelted <- melt(DfForHM)
+
+names(DfMelted)[names(DfMelted) == "Unnamed..0"] <- "Coordinate"
+names(DfMelted)[names(DfMelted) == "variable"] <- "Topics"
+
+pdf("HeatmapTop20percentProbes.pdf", height=10, width=15)
+
+heatmap <- ggplot(DfMelted, aes(Topics, Coordinate, fill= value)) + 
+ geom_tile()
+
+heatmap
 
 dev.off()
