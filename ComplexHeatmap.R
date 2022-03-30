3e59f0b542c5365db8f63450ed7d1df790e59d7b
c00kieraptor <peterarnesen@outlook.com>
2022-03-30 22:37:02 +0200

    Added commented dataload for manual check

--- ComplexHeatmap.R
+++ ComplexHeatmap.R
@@ -7,4 +7,5 @@ library(ComplexHeatmap)
 df <- read.table(snakemake@input[["py_DF"]], header=TRUE, sep="\t")
 meta <- read.table(snakemake@input[["py_meta"]], header=TRUE, sep="\t")
 
-# df <- read.table("py_df.csv", header=TRUE, sep="\t")
+# df <- read.table("py_df.csv", header=TRUE, sep=",")
+# meta <- read.table("py_df.csv", header=TRUE, sep="\t")
