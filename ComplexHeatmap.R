88b6338641542c7f6039ae79c9588d166f49f9b4
c00kieraptor <peterarnesen@outlook.com>
2022-03-30 22:16:18 +0200

    DF too large for seaborn - transferring finished DF to R and complexHeatmap

---
+++ ComplexHeatmap.R
@@ -0,0 +1,10 @@
+
+#Load libraries
+library(ComplexHeatmap)
+
+
+#Load data
+df <- read.table(snakemake@input[["py_DF"]], header=TRUE, sep="\t")
+meta <- read.table(snakemake@input[["py_meta"]], header=TRUE, sep="\t")
+
+# df <- read.table("py_df.csv", header=TRUE, sep="\t")
